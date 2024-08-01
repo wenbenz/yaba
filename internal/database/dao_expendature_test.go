877601e4271d21bdea107e446548ecc176ef5e76
Ben Zhao <ben.zhao@benzhao.dev>
2024-07-31 20:33:15 -0400

    Expendatures (#7)
    * added expenditures
    
    * expendatures go files

---
+++ internal/database/dao_expendature_test.go
@@ -0,0 +1,73 @@
+package database_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"yaba/internal/budget"
+	"yaba/internal/database"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestExpenditures(t *testing.T) {
+	t.Parallel()
+
+	pool, cleanupFunc := SetupTestContainerAndInitPool()
+	defer cleanupFunc()
+
+	ctx := context.Background()
+
+	// Create expenditures
+	numExpenditures := 50
+	owner, _ := uuid.NewRandom()
+	expenditures := make([]*budget.Expenditure, numExpenditures)
+
+	for i := range numExpenditures {
+		//nolint:exhaustruct
+		expenditures[i] = &budget.Expenditure{
+			Owner:          owner,
+			Name:           fmt.Sprintf("expenditure %d", i),
+			Amount:         (i * 123) % 400,
+			Date:           time.Now().Add(time.Duration(i-numExpenditures) * time.Hour),
+			Method:         "cash",
+			BudgetCategory: "spending",
+		}
+	}
+
+	require.NoError(t, database.PersistExpenditures(ctx, pool, expenditures))
+
+	// Fetch newly created expenditures
+	fetched, err := database.ListExpenditures(ctx, pool, owner,
+		time.Now().Add(time.Duration(-52)*time.Hour), time.Now(), 100)
+	require.NoError(t, err)
+	require.Len(t, fetched, numExpenditures)
+
+	// Check that they are the same
+	for i, actual := range fetched {
+		expected := expenditures[i]
+		require.Equal(t, expected.Owner, actual.Owner)
+		require.Equal(t, expected.Name, actual.Name)
+		require.Equal(t, expected.Amount, actual.Amount)
+		require.Equal(t, expected.Date.Format(time.RFC3339), actual.Date.Format(time.RFC3339))
+		require.Equal(t, expected.BudgetCategory, actual.BudgetCategory)
+		require.Equal(t, expected.RewardCategory, actual.RewardCategory)
+		require.Equal(t, expected.Method, actual.Method)
+		require.Equal(t, expected.Comment, actual.Comment)
+	}
+
+	// Fetch with smaller limit
+	fetched, err = database.ListExpenditures(ctx, pool, owner, expenditures[0].Date, time.Now(), 10)
+	require.NoError(t, err)
+	require.Equal(t, expenditures[0].Name, fetched[0].Name)
+	require.Equal(t, expenditures[9].Name, fetched[9].Name)
+
+	// Fetch with time range
+	fetched, err = database.ListExpenditures(ctx, pool, owner, fetched[4].Date, fetched[8].Date, 10)
+	require.NoError(t, err)
+	require.Equal(t, expenditures[4].Name, fetched[0].Name)
+	require.Equal(t, expenditures[8].Name, fetched[4].Name)
+}
