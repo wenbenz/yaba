edeeb4cdc78c02fe2b48a9e13288579bd7b598bc
Ben <ben.zhao@benzhao.dev>
2024-08-12 15:55:52 -0400

    change the migration for crete expenditure to add an enum -- nobody's using this right now

--- migrations/000002_create_expendatures.up.sql
+++ migrations/000002_create_expendatures.up.sql
@@ -1,3 +1,20 @@
+/* Sources:
+ *  - https://forums.redflagdeals.com/ultimate-tangerine-mc-category-reference-guide-2278771/
+ *  - https://www.tangerine.ca/en/personal/spend/credit-cards/money-back-credit-card
+ */
+CREATE TYPE reward_category AS ENUM (
+    'DRUG_STORE',
+    'ENTERTAINMENT',
+    'FURNITURE',
+    'GAS',
+    'GROCERY',
+    'HOME_IMPROVEMENT',
+    'HOTEL',
+    'PUBLIC_TRANSPORTATION',
+    'RECURRING_BILL',
+    'RESTAURANT'
+);
+
 CREATE TABLE IF NOT EXISTS expenditure (
     id SERIAL PRIMARY KEY,
     owner UUID NOT NULL,
@@ -6,7 +23,7 @@ CREATE TABLE IF NOT EXISTS expenditure (
     name VARCHAR(50),
     method VARCHAR(50),
     budget_category VARCHAR(20),
-    reward_category VARCHAR(20),
+    reward_category reward_category,
     comment TEXT
 );
 
