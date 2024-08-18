63116990b605fa87e00912bc70721d15a590e5dc
Ben Zhao <ben.zhao@benzhao.dev>
2024-08-18 15:43:06 -0400

    support multiple file uploads from the UI (#26)
    * support multiple file uploads from the UI
    
    * refactor

--- internal/handlers/upload_handler.go
+++ internal/handlers/upload_handler.go
@@ -1,8 +1,12 @@
 package handlers
 
 import (
+	"context"
+	"encoding/json"
 	"log"
+	"mime/multipart"
 	"net/http"
+	"sync"
 	"yaba/internal/constants"
 	"yaba/internal/platform"
 
@@ -10,8 +14,19 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+const (
+	defaultMemory = 32 << 20
+	expenditures  = "expenditures[]"
+)
+
 type UploadHandler struct {
 	Pool *pgxpool.Pool
+}
+
+type uploadErrors map[string]string
+type uploadError struct {
+	filename string
+	err      string
 }
 
 func (h UploadHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -24,26 +39,87 @@ func (h UploadHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	log.Println("handling upload for user " + user.String())
 
-	file, header, err := r.FormFile("myFile")
+	if err := r.ParseMultipartForm(defaultMemory); err != nil { // 32 MB -- default from request.go
+		log.Println("Error parsing the file: ", err)
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte(err.Error()))
+
+		return
+	}
+
+	if fileHeaders, ok := r.MultipartForm.File[expenditures]; ok {
+		h.handleExpenditureCSV(r.Context(), fileHeaders, user, w)
+
+		return
+	}
+
+	w.WriteHeader(http.StatusUnprocessableEntity)
+}
+
+func (h UploadHandler) handleExpenditureCSV(ctx context.Context,
+	fileHeaders []*multipart.FileHeader, user uuid.UUID, w http.ResponseWriter) {
+	wg := sync.WaitGroup{}
+	failed := make(chan uploadError)
+
+	// Upload expenditure for each file.
+	for _, fh := range fileHeaders {
+		go h.uploadExpenditure(ctx, fh, user, &wg, failed)
+		wg.Add(1)
+	}
+
+	// Go routine to collect failures.
+	failures := make(chan uploadErrors, 1)
+	go func() {
+		ues := uploadErrors{}
+		for ue := range failed {
+			ues[ue.filename] = ue.err
+		}
+
+		failures <- ues
+		close(failures)
+	}()
+
+	// Once all the upload routines have completed, close the failures channel and check failures.
+	wg.Wait()
+	close(failed)
+
+	allFailures := <-failures
+
+	if len(allFailures) != 0 {
+		w.WriteHeader(http.StatusBadRequest)
+
+		if responseObject, err := json.Marshal(allFailures); err == nil {
+			_, _ = w.Write(responseObject)
+		}
+
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+// Reads the CSV from fh and writes it to the expenditures table.
+// Failures.
+func (h UploadHandler) uploadExpenditure(ctx context.Context, fh *multipart.FileHeader, user uuid.UUID,
+	wg *sync.WaitGroup, failed chan<- uploadError) {
+	defer wg.Done()
+
+	filename := fh.Filename
+	file, err := fh.Open()
+
 	if err != nil {
-		log.Println("Error Retrieving the File")
-		log.Println(err)
-		w.WriteHeader(http.StatusUnprocessableEntity)
+		log.Println("Error opening the file: ", err)
+		failed <- uploadError{filename: filename, err: err.Error()}
 
 		return
 	}
 
 	defer file.Close()
 
-	err = platform.UploadSpendingsCSV(r.Context(), h.Pool, user, file, header.Filename)
-	if err != nil {
-		log.Println("Error reading CSV")
-		log.Println(err)
-		w.WriteHeader(http.StatusUnprocessableEntity)
-		_, _ = w.Write([]byte(err.Error()))
+	if err = platform.UploadSpendingsCSV(ctx, h.Pool, user, file, filename); err != nil {
+		log.Println("Error reading CSV: ", err)
+		failed <- uploadError{filename: filename, err: err.Error()}
 
 		return
 	}
-
-	w.WriteHeader(http.StatusAccepted)
 }
--- test/handlers/upload_handler_test.go
+++ test/handlers/upload_handler_test.go
@@ -14,7 +14,9 @@ import (
 	"os"
 	"strconv"
 	"testing"
+	"time"
 	"yaba/internal/constants"
+	"yaba/internal/database"
 	"yaba/internal/handlers"
 	"yaba/test/helper"
 
@@ -30,7 +32,7 @@ func TestUploadNoUser(t *testing.T) {
 	pool := helper.GetTestPool()
 	handler := handlers.UploadHandler{Pool: pool}
 
-	request, err := UploadCSVRequest("testdata/spend.csv")
+	request, err := UploadCSVRequest([]string{"testdata/spend.csv"})
 	require.NoError(t, err)
 
 	handler.ServeHTTP(w, request)
@@ -47,13 +49,19 @@ func TestUploadCSV(t *testing.T) {
 	pool := helper.GetTestPool()
 	handler := handlers.UploadHandler{Pool: pool}
 
-	request, err := UploadCSVRequest("testdata/spend.csv")
+	request, err := UploadCSVRequest([]string{"testdata/spend.csv", "testdata/spend2.csv"})
 	require.NoError(t, err)
 
 	request = request.WithContext(context.WithValue(context.Background(), constants.CTXUser, user))
 
 	handler.ServeHTTP(w, request)
-	require.Equal(t, http.StatusAccepted, w.Code)
+	require.Equal(t, http.StatusOK, w.Code)
+
+	// Check that rows in each csv exist
+	date := time.Date(2006, time.July, 8, 0, 0, 0, 0, time.UTC)
+	expenditures, err := database.ListExpenditures(context.Background(), pool, user, date, date, 100)
+	require.NoError(t, err)
+	require.Len(t, expenditures, 4)
 }
 
 func TestUploadNotCSV(t *testing.T) {
@@ -66,13 +74,17 @@ func TestUploadNotCSV(t *testing.T) {
 	pool := helper.GetTestPool()
 	handler := handlers.UploadHandler{Pool: pool}
 
-	request, err := UploadCSVRequest("testdata/file.txt")
+	request, err := UploadCSVRequest([]string{"testdata/file.txt"})
 	require.NoError(t, err)
 
 	request = request.WithContext(context.WithValue(context.Background(), constants.CTXUser, user))
 
 	handler.ServeHTTP(w, request)
-	require.Equal(t, http.StatusUnprocessableEntity, w.Code)
+	require.Equal(t, http.StatusBadRequest, w.Code)
+	require.Equal(t, `{"file.txt":"failed to import: `+
+		`unrecognized column 'I'm a file.' in headers: `+
+		`invalid input value: [I'm a file.]"}`,
+		w.Body.String())
 }
 
 func TestUploadWrongKey(t *testing.T) {
@@ -85,7 +97,7 @@ func TestUploadWrongKey(t *testing.T) {
 	pool := helper.GetTestPool()
 	handler := handlers.UploadHandler{Pool: pool}
 
-	request, err := UploadFileRequest("testdata/file.txt", "text/csv", "foobar")
+	request, err := UploadFileRequest([]string{"testdata/file.txt"}, "text/csv", "foobar")
 	require.NoError(t, err)
 
 	request = request.WithContext(context.WithValue(context.Background(), constants.CTXUser, user))
@@ -94,44 +106,79 @@ func TestUploadWrongKey(t *testing.T) {
 	require.Equal(t, http.StatusUnprocessableEntity, w.Code)
 }
 
-func UploadCSVRequest(filepath string) (*http.Request, error) {
-	return UploadFileRequest(filepath, "text/csv", "myFile")
+func TestUploadCSVPartialSuccess(t *testing.T) {
+	t.Parallel()
+
+	user := uuid.New()
+
+	w := httptest.NewRecorder()
+
+	pool := helper.GetTestPool()
+	handler := handlers.UploadHandler{Pool: pool}
+
+	request, err := UploadCSVRequest([]string{"testdata/spend.csv", "testdata/file.txt"})
+	require.NoError(t, err)
+
+	request = request.WithContext(context.WithValue(context.Background(), constants.CTXUser, user))
+
+	handler.ServeHTTP(w, request)
+	require.Equal(t, http.StatusBadRequest, w.Code)
+	require.Equal(t, `{"file.txt":"failed to import: `+
+		`unrecognized column 'I'm a file.' in headers: `+
+		`invalid input value: [I'm a file.]"}`,
+		w.Body.String())
+
+	// Check that rows in the CSV
+	date := time.Date(2006, time.July, 8, 0, 0, 0, 0, time.UTC)
+	expenditures, err := database.ListExpenditures(context.Background(), pool, user, date, date, 100)
+	require.NoError(t, err)
+	require.Len(t, expenditures, 3)
 }
 
-func UploadFileRequest(filepath, contentType, formKey string) (*http.Request, error) {
-	csvFile, err := os.Open(filepath)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read file %s: %w", filepath, err)
-	}
+func UploadCSVRequest(filepath []string) (*http.Request, error) {
+	return UploadFileRequest(filepath, "text/csv", "expenditures[]")
+}
 
-	stat, err := csvFile.Stat()
-	if err != nil {
-		return nil, fmt.Errorf("failed to stat file: %w", err)
-	}
-
-	hdr := make(textproto.MIMEHeader)
-	cd := mime.FormatMediaType("form-data", map[string]string{
-		"id":       formKey,
-		"name":     formKey,
-		"filename": filepath,
-	})
-	hdr.Set("Content-Disposition", cd)
-	hdr.Set("Contnt-Type", contentType)
-	hdr.Set("Content-Length", strconv.FormatInt(stat.Size(), 10))
-
+func UploadFileRequest(filepaths []string, contentType, formKey string) (*http.Request, error) {
 	var buf bytes.Buffer
 
 	mw := multipart.NewWriter(&buf)
 	defer mw.Close()
 
-	part, err := mw.CreatePart(hdr)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create new form part: %w", err)
-	}
+	var contentLength int64
 
-	csvLen, err := io.Copy(part, csvFile)
-	if err != nil {
-		return nil, fmt.Errorf("failed to write form part: %w", err)
+	for _, filepath := range filepaths {
+		csvFile, err := os.Open(filepath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read file %s: %w", filepath, err)
+		}
+
+		stat, err := csvFile.Stat()
+		if err != nil {
+			return nil, fmt.Errorf("failed to stat file: %w", err)
+		}
+
+		hdr := make(textproto.MIMEHeader)
+		cd := mime.FormatMediaType("form-data", map[string]string{
+			"id":       formKey,
+			"name":     formKey,
+			"filename": filepath,
+		})
+		hdr.Set("Content-Disposition", cd)
+		hdr.Set("Contnt-Type", contentType)
+		hdr.Set("Content-Length", strconv.FormatInt(stat.Size(), 10))
+
+		part, err := mw.CreatePart(hdr)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create new form part: %w", err)
+		}
+
+		csvLen, err := io.Copy(part, csvFile)
+		if err != nil {
+			return nil, fmt.Errorf("failed to write form part: %w", err)
+		}
+
+		contentLength += csvLen
 	}
 
 	url, _ := url.Parse("http://localhost:8080/upload")
@@ -144,6 +191,6 @@ func UploadFileRequest(filepath, contentType, formKey string) (*http.Request, er
 		URL:           url,
 		Header:        header,
 		Body:          io.NopCloser(&buf),
-		ContentLength: csvLen,
+		ContentLength: contentLength,
 	}, nil
 }
