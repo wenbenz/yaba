c7191c430e996fa7f21b0129d1146682eeed0854
Ben <ben.zhao@benzhao.dev>
2024-08-18 02:06:20 -0400

    restructure graphql code

--- internal/handlers/graphql.go
+++ internal/handlers/graphql.go
@@ -6,24 +6,25 @@ import (
 	"github.com/graphql-go/graphql"
 )
 
+var ping = &graphql.Field {
+	Type: graphql.String,
+	Name: "Ping",
+	Description: "Responds 'pong' to ping",
+	Resolve: func(_ graphql.ResolveParams) (interface{}, error) {
+		return "pong", nil
+	},
+}
+
 func CreateGraphqlSchema() (*graphql.Schema, error) {
-	fields := graphql.Fields{
-		"ping": &graphql.Field{
-			Type: graphql.String,
-			Name: "Ping",
-			Resolve: func(_ graphql.ResolveParams) (interface{}, error) {
-				return "pong", nil
-			},
-		},
-	}
-
-	rootQuery := graphql.ObjectConfig{
-		Name:   "RootQuery",
-		Fields: fields,
-	}
-
 	schemaConfig := graphql.SchemaConfig{
-		Query: graphql.NewObject(rootQuery),
+		Query: graphql.NewObject(graphql.ObjectConfig{
+			Name:   "RootQuery",
+			Description: "Root query for GraphQL requests whose fields are query paths.",
+			Fields: graphql.Fields{
+				"ping": ping,
+				// add new paths here
+			},
+		}),
 	}
 
 	schema, err := graphql.NewSchema(schemaConfig)
