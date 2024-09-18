80abe901fca37cfc9688f0f38923e8422ec8b401
Ben <ben.zhao@benzhao.dev>
2024-09-17 23:41:47 -0400

    lint

--- internal/handlers/single_user_mode_interceptor.go
+++ internal/handlers/single_user_mode_interceptor.go
@@ -20,7 +20,9 @@ func (interceptor SingleUserModeInterceptor) ServeHTTP(w http.ResponseWriter, r 
 
 	w.Header().Add("Access-Control-Allow-Origin", "*")
 	w.Header().Add("Access-Control-Allow-Credentials", "true")
-	w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	w.Header().Add("Access-Control-Allow-Headers",
+		"Content-Type, Content-Length, Accept-Encoding,"+
+			" X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 	w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 
 	interceptor.Intercepted.ServeHTTP(w, r.WithContext(ctx))
