4cdd3124b00e93b07a87528471992e41f45d046c
Ben Zhao <ben.zhao@benzhao.dev>
2025-03-20 17:49:02 -0400

    added logout handler (#44)
    * added logout handler
    
    * lint
    
    * more tests
    
    * remove test case because it interferes with other tests

---
+++ internal/auth/logout_handler_test.go
@@ -0,0 +1,84 @@
+package auth_test
+
+import (
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/net/context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+	"yaba/internal/auth"
+	"yaba/internal/ctxutil"
+	"yaba/internal/test/helper"
+)
+
+func TestLogoutHandler(t *testing.T) {
+	t.Parallel()
+
+	pool := helper.GetTestPool()
+	user := uuid.New()
+	token := auth.NewSessionToken(user, time.Hour)
+
+	// Save the token
+	err := auth.SaveSessionToken(context.Background(), pool, token)
+	require.NoError(t, err)
+
+	// Create a request with the user context
+	req := httptest.NewRequest(http.MethodPost, "/logout", nil)
+	req = req.WithContext(ctxutil.WithUser(req.Context(), user))
+	cookie, _ := auth.BakeCookie(token, "host")
+	req.AddCookie(cookie)
+
+	// Create a response recorder
+	rr := httptest.NewRecorder()
+
+	// Create the handler
+	handler := auth.NewLogoutHandler(pool)
+
+	// Serve the request
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code
+	require.Equal(t, http.StatusOK, rr.Code)
+
+	// Check that the session token is deleted
+	token, err = auth.GetSessionToken(context.Background(), pool, token.ID)
+	require.Nil(t, token)
+	require.ErrorContains(t, err, "failed to retrieve session token")
+
+	// Check that the session cookie is cleared
+	cookie = rr.Result().Cookies()[0] // nolint:bodyclose
+
+	require.Equal(t, "sid", cookie.Name)
+	require.Equal(t, "", cookie.Value)
+	require.Equal(t, -1, cookie.MaxAge)
+}
+
+func TestLogoutHandler_InvalidSessionCookie(t *testing.T) {
+	t.Parallel()
+
+	pool := helper.GetTestPool()
+	user := uuid.New()
+
+	// Create a request with the user context and an invalid session cookie
+	req := httptest.NewRequest(http.MethodPost, "/logout", nil)
+	req = req.WithContext(ctxutil.WithUser(req.Context(), user))
+	invalidCookie := &http.Cookie{
+		Name:  "sid",
+		Value: "invalid",
+	}
+	req.AddCookie(invalidCookie)
+
+	// Create a response recorder
+	rr := httptest.NewRecorder()
+
+	// Create the handler
+	handler := auth.NewLogoutHandler(pool)
+
+	// Serve the request
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code
+	require.Equal(t, http.StatusUnauthorized, rr.Code)
+}
