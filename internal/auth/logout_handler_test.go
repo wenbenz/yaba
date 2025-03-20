819b7fb3cced8b94ae1d14e185db88a24a53d678
Ben <ben.zhao@benzhao.dev>
2025-03-20 17:46:42 -0400

    remove test case because it interferes with other tests

--- internal/auth/logout_handler_test.go
+++ internal/auth/logout_handler_test.go
@@ -82,32 +82,3 @@ func TestLogoutHandler_InvalidSessionCookie(t *testing.T) {
 	// Check the status code
 	require.Equal(t, http.StatusUnauthorized, rr.Code)
 }
-
-func TestLogoutHandler_ErrorDeletingSessionToken(t *testing.T) {
-	t.Parallel()
-
-	pool := helper.GetTestPool()
-	user := uuid.New()
-	token := auth.NewSessionToken(user, time.Hour)
-
-	// Create a request with the user context and a valid session cookie
-	req := httptest.NewRequest(http.MethodPost, "/logout", nil)
-	req = req.WithContext(ctxutil.WithUser(req.Context(), user))
-	cookie, _ := auth.BakeCookie(token, "host")
-	req.AddCookie(cookie)
-
-	// Create a response recorder
-	rr := httptest.NewRecorder()
-
-	// Create the handler
-	handler := auth.NewLogoutHandler(pool)
-
-	// Simulate an error by closing the pool
-	pool.Close()
-
-	// Serve the request
-	handler.ServeHTTP(rr, req)
-
-	// Check the status code
-	require.Equal(t, http.StatusInternalServerError, rr.Code)
-}
