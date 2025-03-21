08c719e44a52309a4682a0c1a3918a45bf0c8863
Ben <ben.zhao@benzhao.dev>
2025-03-21 18:00:03 -0400

    style

--- internal/import/csv_import.go
+++ internal/import/csv_import.go
@@ -15,9 +15,103 @@ import (
 	"yaba/internal/model"
 )
 
+func UploadSpendingsCSV(ctx context.Context, pool *pgxpool.Pool, user uuid.UUID, data io.Reader, source string) error {
+	csvReader := csv.NewReader(data)
+	expenditures, err := ImportExpendituresFromCSVReader(user, csvReader)
+
+	if err != nil {
+		return fmt.Errorf("failed to import: %w", err)
+	}
+
+	for _, e := range expenditures {
+		e.Source = source
+	}
+
+	if err = database.PersistExpenditures(ctx, pool, expenditures); err != nil {
+		return fmt.Errorf("failed to save: %w", err)
+	}
+
+	return nil
+}
+
+func ImportExpendituresFromCSVReader(owner uuid.UUID, r *csv.Reader) ([]*model.Expenditure, error) {
+	headers, err := r.Read()
+	if err != nil {
+		return nil, fmt.Errorf("received error reading headers: %w", err)
+	}
+
+	expenditureReader, err := NewCSVExpenditureReader(owner, headers)
+	if err != nil {
+		return nil, err
+	}
+
+	var expenditures []*model.Expenditure
+
+	for row, err := r.Read(); err != io.EOF; row, err = r.Read() {
+		if err != nil {
+			return nil, fmt.Errorf("unexpected error reading csv: %w", err)
+		}
+
+		if len(row) == 0 {
+			continue
+		}
+
+		expenditure, err := expenditureReader.ReadRow(row)
+		if err != nil {
+			return nil, err
+		}
+
+		expenditures = append(expenditures, expenditure)
+	}
+
+	return expenditures, err
+}
+
 type CsvExpenditureReader struct {
 	header2index map[string]int
 	owner        uuid.UUID
+}
+
+func NewCSVExpenditureReader(owner uuid.UUID, headers []string) (*CsvExpenditureReader, error) {
+	reader := CsvExpenditureReader{
+		owner:        owner,
+		header2index: make(map[string]int),
+	}
+
+	if err := validateHeaders(headers); err != nil {
+		return nil, err
+	}
+
+	for i, h := range headers {
+		reader.header2index[h] = i
+	}
+
+	return &reader, nil
+}
+
+func (reader *CsvExpenditureReader) ReadRow(row []string) (*model.Expenditure, error) {
+	date, err := reader.getDate(row, "date")
+	if err != nil {
+		return nil, err
+	}
+
+	amount, err := reader.getFloat64(row, "amount")
+	if err != nil {
+		return nil, err
+	}
+
+	rewardCategory := reader.getString(row, "reward_category")
+
+	return &model.Expenditure{
+		Owner:          reader.owner,
+		Name:           reader.getString(row, "name"),
+		Date:           date,
+		Amount:         amount,
+		Method:         reader.getString(row, "method"),
+		BudgetCategory: reader.getString(row, "budget_category"),
+		RewardCategory: strings.ToUpper(rewardCategory),
+		Comment:        reader.getString(row, "comment"),
+	}, nil
 }
 
 func (reader *CsvExpenditureReader) getString(row []string, key string) string {
@@ -50,48 +144,6 @@ func (reader *CsvExpenditureReader) getFloat64(row []string, key string) (float6
 	}
 
 	return dollars, nil
-}
-
-func (reader *CsvExpenditureReader) ReadRow(row []string) (*model.Expenditure, error) {
-	date, err := reader.getDate(row, "date")
-	if err != nil {
-		return nil, err
-	}
-
-	amount, err := reader.getFloat64(row, "amount")
-	if err != nil {
-		return nil, err
-	}
-
-	rewardCategory := reader.getString(row, "reward_category")
-
-	return &model.Expenditure{
-		Owner:          reader.owner,
-		Name:           reader.getString(row, "name"),
-		Date:           date,
-		Amount:         amount,
-		Method:         reader.getString(row, "method"),
-		BudgetCategory: reader.getString(row, "budget_category"),
-		RewardCategory: strings.ToUpper(rewardCategory),
-		Comment:        reader.getString(row, "comment"),
-	}, nil
-}
-
-func NewCSVExpenditureReader(owner uuid.UUID, headers []string) (*CsvExpenditureReader, error) {
-	reader := CsvExpenditureReader{
-		owner:        owner,
-		header2index: make(map[string]int),
-	}
-
-	if err := validateHeaders(headers); err != nil {
-		return nil, err
-	}
-
-	for i, h := range headers {
-		reader.header2index[h] = i
-	}
-
-	return &reader, nil
 }
 
 func validateHeaders(headers []string) error {
@@ -127,61 +179,6 @@ func validateHeaders(headers []string) error {
 
 	if !hasAmount {
 		return fmt.Errorf("missing required column 'amount': %w", errors.InvalidInputError{Input: headers})
-	}
-
-	return nil
-}
-
-func ImportExpendituresFromCSVReader(owner uuid.UUID, r *csv.Reader) ([]*model.Expenditure, error) {
-	// First row is always headers.
-	headers, err := r.Read()
-
-	if err != nil {
-		return nil, fmt.Errorf("received error reading headers: %w", err)
-	}
-
-	expenditureReader, err := NewCSVExpenditureReader(owner, headers)
-	if err != nil {
-		return nil, err
-	}
-
-	var expenditures []*model.Expenditure
-
-	for row, err := r.Read(); err != io.EOF; row, err = r.Read() {
-		if err != nil {
-			return nil, fmt.Errorf("unexpected error reading csv: %w", err)
-		}
-
-		// skip empty
-		if len(row) == 0 {
-			continue
-		}
-
-		expenditure, err := expenditureReader.ReadRow(row)
-		if err != nil {
-			return nil, err
-		}
-
-		expenditures = append(expenditures, expenditure)
-	}
-
-	return expenditures, err
-}
-
-func UploadSpendingsCSV(ctx context.Context, pool *pgxpool.Pool, user uuid.UUID, data io.Reader, source string) error {
-	csvReader := csv.NewReader(data)
-	expenditures, err := ImportExpendituresFromCSVReader(user, csvReader)
-
-	if err != nil {
-		return fmt.Errorf("failed to import: %w", err)
-	}
-
-	for _, e := range expenditures {
-		e.Source = source
-	}
-
-	if err = database.PersistExpenditures(ctx, pool, expenditures); err != nil {
-		return fmt.Errorf("failed to save: %w", err)
 	}
 
 	return nil
